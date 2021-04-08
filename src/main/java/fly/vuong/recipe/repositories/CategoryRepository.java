c07a3527a6563d07543a77157bde668ee5cab4bd
vuongfly <vuongfly96@gmail.com>
2021-04-08 18:15:13 +0700

    edit code

--- src/main/java/fly/vuong/recipe/repositories/CategoryRepository.java
+++ src/main/java/fly/vuong/recipe/repositories/CategoryRepository.java
@@ -1,6 +1,7 @@
 package fly.vuong.recipe.repositories;
 
 import fly.vuong.recipe.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
