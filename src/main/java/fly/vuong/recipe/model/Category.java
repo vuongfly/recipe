e07b415c123af1a68f6c4b3de1874ba0177ca07e
vuongfly <vuongfly96@gmail.com>
2021-04-08 17:52:00 +0700

    update relationship between Recipe and Category

---
+++ src/main/java/fly/vuong/recipe/model/Category.java
@@ -0,0 +1,22 @@
+package fly.vuong.recipe.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+
+}
--- src/main/java/fly/vuong/recipe/model/Recipe.java
+++ src/main/java/fly/vuong/recipe/model/Recipe.java
@@ -32,4 +32,10 @@ public class Recipe {
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 }
