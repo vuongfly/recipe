15cd2450fe228a52622124f569fa7de137cbf458
vuongfly <vuongfly96@gmail.com>
2021-04-09 17:19:37 +0700

    update code

--- src/main/java/fly/vuong/recipe/repositories/CategoryRepository.java
+++ src/main/java/fly/vuong/recipe/repositories/CategoryRepository.java
@@ -4,5 +4,8 @@ import fly.vuong.recipe.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+    Optional<Category> findByDescription(String description);
 }
